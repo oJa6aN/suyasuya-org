0e34b509a07b45ba1ee7f271073a58d288b77238
oJa6aN <mrg0zp@gmail.com>
2024-10-24 04:30:24 +0900

    refactor:

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -2,20 +2,25 @@
 
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime"
+
+const handleRedirect = (router: AppRouterInstance) => {
+  const now = new Date()
+  const currentHour = now.getHours()
+
+  if (22 <= currentHour || currentHour < 5) {
+    // May peaceful slumber be upon thee.
+    router.push('https://note.com/minerva_owl/n/nba7b0afef16c')
+  } else {
+    router.push('https://note.com/minerva_owl/n/nf58e9c59b1e9')
+  }
+}
 
 export default function Home() {
   const router = useRouter()
 
   useEffect(() => {
-    const now = new Date()
-    const currentHour = now.getHours()
-    console.log(currentHour)
-    if (22 <= currentHour || currentHour < 5) {
-      // May peaceful slumber be upon thee.
-      router.push('https://note.com/minerva_owl/n/nba7b0afef16c')
-    } else {
-      router.push('https://note.com/minerva_owl/n/nf58e9c59b1e9')
-    }
+    handleRedirect(router)
   }, [router])
 
   return (
