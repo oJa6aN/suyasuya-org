e9793e5425d68133ff6cacc25abf35371755943d
oJa6aN <mrg0zp@gmail.com>
2024-10-24 04:23:56 +0900

    feat: top-page

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,18 +1,25 @@
-import { GetServerSideProps } from 'next'
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const now = new Date()
+    const currentHour = now.getHours()
+    console.log(currentHour)
+    if (22 <= currentHour || currentHour < 5) {
+      // May peaceful slumber be upon thee.
+      router.push('https://note.com/minerva_owl/n/nba7b0afef16c')
+    } else {
+      router.push('https://note.com/minerva_owl/n/nf58e9c59b1e9')
+    }
+  }, [router])
+
   return (
     <>
-      <p>suyasuya</p>
     </>
   )
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination: 'https://note.com/minerva_owl/n/nba7b0afef16c',
-      permanent: true
-    },
-  }
 }
